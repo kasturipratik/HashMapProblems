43e86c7e0776bf131e091dd79966039c39ff4bd3
kasturipratik <pratik.parajuli@yahoo.com>
2018-06-06 14:08:38 -0400

    works

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.HashMap;
 import java.util.Random;
 import java.util.Scanner;
@@ -39,8 +41,16 @@ public class Main {
 
            String changedQuestion =changesFromHashMap(question,replacements,hedges,qualify);
            String allCaps = allCaps(changedQuestion);
+           String pigLatin =pigLatin(changedQuestion);
            if(question.equals("caps")) {
                System.out.println(allCaps);
+           }
+           else if(question.equals("pig"))
+           {
+               System.out.println(pigLatin);
+           }
+           else if(question.equals("play game")){
+               playGame();
            }
            else{
                System.out.println(changedQuestion);
@@ -124,18 +134,103 @@ public static  String changesFromHashMap(String question, HashMap<String, String
  //pig latin method
 
  public static String pigLatin(String response){
+        //spliting the final string of the project
         String [] pigChange = response.split(" ");
+        //declaring a array variable
+        String[] rejoin = new String[pigChange.length];
+        String singleString="";
+        //for loop to change the string value of each letter to pig lating
         for(int i =0; i<pigChange.length;i++)
         {
+            // storing the changed values of pig latin in an array
+            rejoin[i] = pigChange[i].substring(1)+pigChange[i].substring(0,1)+"py";
+
+        }
+        // for loop to join all the array to a string
+        for(int i =0; i <rejoin.length;i++)
+        {
+            singleString += rejoin[i]+" ";
+        }
+       // returning the chhanged single string
+return singleString;
+
+ }
+
+    public static void playGame(){
+        Scanner input = new Scanner(System.in);
+        String firstLevel,secondLevel,thirdLevel,fourthLevel;
+        System.out.println("WELCOME TO MITCHELL'S TINY ADVENTURE!");
+        System.out.println("You are in a creepy house!  Would you like to go \"upstairs\" or into the \"kitchen\"?");
+        firstLevel = input.nextLine();
+
+        if(firstLevel.equals("kitchen")){
+            System.out.println("There is a long countertop with dirty dishes everywhere.  Off to one side\n" +
+                    "there is, as you'd expect, a refrigerator. You may open the \"refrigerator\"\n" +
+                    "or look in a \"cabinet\".");
+            secondLevel = input.nextLine();
+
+            if(secondLevel.equals("refrigerator")){
+                System.out.println("Inside the refrigerator you see food and stuff.  It looks pretty nasty.\n" +
+                        "Would you like to eat some of the food? (\"yes\" or \"no\")");
+                thirdLevel = input.nextLine();
+
+                if(thirdLevel.equals("no")){
+                    System.out.println("You die of starvation... eventually.");
+                }
+                else if(thirdLevel.equals("yes")){
+                    System.out.println("Enjoy the pizza inside the refrigerator. It is fresh. I was kidding earlier.");
+                }
+            }
+            else if(secondLevel.equals("cabinet")){
+                System.out.println("You are in the cabinet. There is a pandora box. Do you want to open it?(\"yes\" or \"no\")");
+                thirdLevel=input.nextLine();
+                if(thirdLevel.equals("yes"))
+                {
+                    System.out.println("You got trapped in the pandora box");
+                }
+                else{
+                    System.out.println("You chose to be lucky. Good Job!!");
+                }
+            }
+        }
+        else if(firstLevel.equals("upstairs"))
+        {
+            System.out.println("Upstairs you see a hallway.  At the end of the hallway is the master\n" +
+                    "\"bedroom\".  There is also a \"bathroom\" off the hallway.  Where would you like\n" +
+                    "to go?");
+            secondLevel=input.nextLine();
+            if(secondLevel.equals("bedroom")){
+                System.out.println("You are in a plush bedroom, with expensive-looking hardwood furniture.  The\n" +
+                        "bed is unmade.  In the back of the room, the closet door is ajar.  Would you\n" +
+                        "like to open the door? (\"yes\" or \"no\")");
+                thirdLevel=input.nextLine();
+                if(thirdLevel.equals("no"))
+                {
+                    System.out.println("Well, then I guess you'll never know what was in there.  Thanks for playing,\n" +
+                            "I'm tired of making nested if statements.");
+                }
+                else if(thirdLevel.equals("yes"))
+                {
+                    System.out.println("Haha, you won the game. ");
+                }
+            }
+            else if(secondLevel.equals("bathroom"))
+            {
+                System.out.println("Welcome to the bathroom level. Do you want to use the sink? (yes/no)");
+                thirdLevel=input.nextLine();
+                if(thirdLevel.equals("yes")){
+                    System.out.println("The water is not working huhhahahahahah");
+                }
+                else if(thirdLevel.equals("no"))
+                {
+                    System.out.println("won the game hahahahahahahahahahahahah");
+                }
+            }
 
         }
 
-
-
- }
-    public static void playGame(String response){
-
     }
     public static void red(String response){}
-
+             JTextArea textbox = new JTextArea("hello world");
+            textbox.setForeground(Color.red)
 }
